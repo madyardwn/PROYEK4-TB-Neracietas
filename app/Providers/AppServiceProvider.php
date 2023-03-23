28a9355fbd89ab9c8ff97e8031b6c8542cebd5da
madyardwn <madyardwn@gmail.com>
2023-03-23 08:13:54 +0700

    Initial Commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\ServiceProvider;
+use Yajra\DataTables\Html\Builder;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        Builder::useVite();
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+
+// Import Tabler Core
+import '@tabler/core/src/js/tabler.js';
+
+// yajra datatables
+import './bootstrap';
+import 'laravel-datatables-vite';
