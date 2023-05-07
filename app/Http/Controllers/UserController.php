5f4b89331f073d07074b10845a966e4b6d6355d3
madyardwn <madyardwn@gmail.com>
2023-05-08 02:55:57 +0700

    delete required na

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,7 +30,7 @@ class UserController extends Controller
     {
         $rules = [
             'nim' => 'required|unique:users,nim|numeric',
-            'na' => 'required|unique:users,na|numeric',
+            'na' => 'unique:users,na|numeric',
             'nama_bagus' => 'required|max:20',
             'year' => 'required|numeric',
             'name' => 'required|max:50',
