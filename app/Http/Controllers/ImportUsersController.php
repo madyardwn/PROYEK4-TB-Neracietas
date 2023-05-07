0158f58adc70a163cf4901ec795726f09470e04a
madyardwn <madyardwn@gmail.com>
2023-05-08 02:45:13 +0700

    add file

--- app/Http/Controllers/ImportUsersController.php
+++ app/Http/Controllers/ImportUsersController.php
@@ -15,9 +15,6 @@ class ImportUsersController extends Controller
 
     public function import(Request $request)
     {
-        $request->validate([
-            'file' => 'required|mimes:xls,xlsx,csv'
-        ]);
 
         try {
             Excel::import(new ImportUsers, $request->file('file'));
--- app/Imports/ImportUsers.php
+++ app/Imports/ImportUsers.php
@@ -15,32 +15,36 @@ class ImportUsers implements ToModel
     public function model(array $row)
     {
         // ignore header or first row
-        if (
-            $row[0] == 'nim' &&
-            $row[1] == 'name' &&
-            $row[2] == 'email' &&
-            $row[3] == 'password' &&
-            $row[4] == 'year'
-        ) return null;
+        if ($row[0] == 'nim'
+            && $row[1] == 'name'
+            && $row[2] == 'email'
+            && $row[3] == 'password'
+            && $row[4] == 'year'
+        ) { return null;
+        }
 
         // check if user already exists just update it
         $user = User::where('nim', $row[0])->first();
         if ($user) {
-            $user->update([
+            $user->update(
+                [
                 'name' => $row[1],
                 'email' => $row[2],
                 'password' => bcrypt($row[3]),
                 'year' => $row[4],
-            ]);
+                ]
+            );
             return null;
         }
 
-        return new User([
+        $user =  new User(
+            [
             'nim' => $row[0],
             'name' => $row[1],
             'email' => $row[2],
             'password' => bcrypt($row[3]),
             'year' => $row[4],
-        ]);
+            ]
+        );
     }
 }
