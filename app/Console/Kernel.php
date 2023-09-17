2f312a222cd38a0b10d970d2c824594dbf4ea932
madyardwn <madyardwn@gmail.com>
2023-09-17 19:38:26 +0700

    fix : change back timezone

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,9 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->command('app:update-events-status')->everyMinute();
-        // time zone indonesia
-        $schedule->command('app:update-events-status')->timezone('Asia/Jakarta')->everyMinute();
+        $schedule->command('app:update-events-status')->everyMinute();
     }
 
     /**
